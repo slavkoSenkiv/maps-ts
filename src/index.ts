44e7db6f8d79215a7e672bae05d57693df263cc1
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-07-19 08:26:37 +0300

    add map but is not working

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 import { User } from "./User";
+import { Company } from "./Company";
+
 console.log('hello there');
+
 const user = new User;
 console.log(user);
 
+const company = new Company;
+console.log(company);
+
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+});
