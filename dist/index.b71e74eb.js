985be57620c4100a2a14587cc761c69d630835aa
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-07-20 08:00:11 +0300

    refractor code - separate google map class into separate file and limit its methods access with private keyword

--- dist/index.b71e74eb.js
+++ dist/index.b71e74eb.js
@@ -584,13 +584,54 @@ console.log(user);
 
 const company = new Company;
 console.log(company);
- */ new google.maps.Map(document.getElementById("map"), {
-    zoom: 1,
-    center: {
-        lat: 0,
-        lng: 0
+ */ var _customMap = require("./CustomMap");
+new (0, _customMap.CustomMap)("map");
+
+},{"./CustomMap":"4qzxZ"}],"4qzxZ":[function(require,module,exports) {
+var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
+parcelHelpers.defineInteropFlag(exports);
+parcelHelpers.export(exports, "CustomMap", ()=>CustomMap);
+class CustomMap {
+    constructor(divId){
+        this.googleMap = new google.maps.Map(document.getElementById(divId), {
+            zoom: 1,
+            center: {
+                lat: 0,
+                lng: 0
+            }
+        });
     }
-});
+}
+
+},{"@parcel/transformer-js/src/esmodule-helpers.js":"l7ZcB"}],"l7ZcB":[function(require,module,exports) {
+exports.interopDefault = function(a) {
+    return a && a.__esModule ? a : {
+        default: a
+    };
+};
+exports.defineInteropFlag = function(a) {
+    Object.defineProperty(a, "__esModule", {
+        value: true
+    });
+};
+exports.exportAll = function(source, dest) {
+    Object.keys(source).forEach(function(key) {
+        if (key === "default" || key === "__esModule" || dest.hasOwnProperty(key)) return;
+        Object.defineProperty(dest, key, {
+            enumerable: true,
+            get: function() {
+                return source[key];
+            }
+        });
+    });
+    return dest;
+};
+exports.export = function(dest, destName, get) {
+    Object.defineProperty(dest, destName, {
+        enumerable: true,
+        get: get
+    });
+};
 
 },{}]},["8qcLR","h7u1C"], "h7u1C", "parcelRequire94c2")
 
--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,5 @@ console.log(user);
 const company = new Company;
 console.log(company);
  */
-new google.maps.Map(document.getElementById('map') as HTMLElement, {
-    zoom: 1,
-    center: {
-        lat: 0,
-        lng: 0
-    }
-});
+import { CustomMap } from "./CustomMap";
+new CustomMap('map');
