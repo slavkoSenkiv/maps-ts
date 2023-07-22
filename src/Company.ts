d6212898707dc524390bf4c2412458c40f1afdaa
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-07-22 07:56:59 +0300

    add **class implement interface** functionality

--- src/Company.ts
+++ src/Company.ts
@@ -1,13 +1,14 @@
 import { faker } from "@faker-js/faker";
+import { Mappable } from "./CustomMap";
 
-
-export class Company {
+export class Company implements Mappable{
     companyName: string;
     catchPhrase: string;
     location: {
         lat: number;
         lng: number;
     };
+    color: string = 'green';
 
     constructor(){
         this.companyName = faker.company.name();
